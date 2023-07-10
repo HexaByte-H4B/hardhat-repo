029e03e6a1ce6c8646cdf38d620a179d8ffe2d11
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-07-10 17:18:36 +0530

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,12 @@ require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const PRIVATE_KEY = 'bb77e6176bad9fa43a769290c08e0ba4f0ab75fccbbb8f59e69782ee4749b54d';
-const API_URL = 'https://rpc-mumbai.maticvigil.com/v1/a34db024b9eb4a61273e3dec51d9a73d227c407b';
+const ZEEVE_API_URL = 'https://app.zeeve.io/shared-api/poly/8522de9e286154c1ca74cd1e9d190f7e7400da9adb3a1f44/';
 module.exports = {
   solidity: "0.8.17",
   networks: {
     polygon_mumbai: {
-      url: API_URL,
+      url: ZEEVE_API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     }
   }
